f665306212e576429e2f1e60dfd634b54dde8e02
Oodan <aidanlhbaros@gmail.com>
2022-08-29 13:06:32 -0600

    spice

---
+++ DC8-29.py
@@ -0,0 +1,10 @@
+import numpy
+a = numpy.array([[1,2,3],
+                [4,5,6]])
+                
+b = numpy.array([[10,11,12],
+                [13,14,15]])
+
+print(a + b)
+print(b - a)
+print(a * 3)
