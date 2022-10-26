0b79d0afbb49fc6c12730ceb541fe652e684a6aa
Oodan <aidanlhbaros@gmail.com>
2022-10-26 12:27:24 -0600

    DC

--- DC10-24-2.py
+++ DC10-24-2.py
@@ -7,8 +7,7 @@ screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 screenX, screenY = screen.get_size()
 
 
-def Fibonacci(a, b, c, d, screenX, screenY, X, Y):
-    check = False
+def Fibonacci(a, b, c, d, X, Y, check):
     xlist = [a, a - b, b * -1, 0]
     ylist = [d * -1, b * -1, 0, a]
     print(a, ", ", b)
@@ -27,9 +26,10 @@ def Fibonacci(a, b, c, d, screenX, screenY, X, Y):
     )
 
     X = X + (xlist[c])
-    if c <= 0 and check != True:
+    if c <= 0 and check == False:
         Y = Y
-        check == True
+        check = True
+        print("test")
     else:
         Y = Y + (ylist[c])
     print(X, ", ", Y)
@@ -38,7 +38,7 @@ def Fibonacci(a, b, c, d, screenX, screenY, X, Y):
     c += 1
     if c == len(xlist):
         c = 0
-    return b, (a + b), c, a, X, Y
+    return b, (a + b), c, a, X, Y, check
 
 
 running = True
@@ -47,8 +47,9 @@ a = 0
 b = 1
 c = -1
 d = 0
-x = 0  #  screenX // 2
-y = 0  # screenY // 2
+x = screenX // 2
+y = screenY // 2
+check = False
 
 while running:
     keys = pygame.key.get_pressed()
@@ -58,6 +59,6 @@ while running:
         if keys[pygame.K_LCTRL]:
             running = False
         if keys[pygame.K_SPACE]:
-            a, b, c, d, x, y = Fibonacci(a, b, c, d, screenX, screenY, x, y)
+            a, b, c, d, x, y, check = Fibonacci(a, b, c, d, x, y, check)
     pygame.display.flip()
 pygame.quit()
